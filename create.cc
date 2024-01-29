13bb1c65ba0d35f53cadf431be4b3383b7fedbd8
Marco Leogrande <dark.knight.ita@gmail.com>
2024-01-28 17:46:52 -0800

    Randomly shuffle created boards

--- create.cc
+++ create.cc
@@ -20,17 +20,48 @@
 
 #include <cstdlib>
 #include <fstream>
+#include <random>
 
 #include "board.h"
 #include "options.h"
 #include "puzzle.h"
+
+// How many random iterations we should perform while shuffling.
+constexpr long RANDOM_ITERATIONS = 100000;
+
+void ShuffleBoard(Board& b, const ProgramOptions& options) {
+  // Create and seed a random number generator
+  std::mt19937 generator;
+  if (options.seed > 0) {
+    generator.seed(options.seed);
+  } else {
+    generator.seed(time(NULL));
+  }
+
+  // Create the distributions that we will use to choose whether to
+  // swap rows and columns, and which specific indices to swap.
+  std::bernoulli_distribution row_column_chooser{0.5};
+  std::uniform_int_distribution<int> index_chooser{1, b.size()};
+  for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+    const int first = index_chooser(generator);
+    const int second = index_chooser(generator);
+
+    if (row_column_chooser(generator)) {
+      // Swap rows
+      b.swap_rows(first, second);
+    } else {
+      // Swap columns
+      b.swap_columns(first, second);
+    }
+  }
+}
 
 int CreateBoard(const ProgramOptions& options) {
   // Create a board.
   Board b{options.board_size, BoardInitializer::DIAGONAL_INCREASING};
 
   // Shuffle randomly based on options
-  // FIXME
+  ShuffleBoard(b, options);
 
   // Print board to the desired location.
   {
